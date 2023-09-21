57f60a8d5ee7ff8e6addc17b6d77ab6f028bc198
Hans Hoffman <hans@oberan.dev>
2023-09-21 09:39:39 -0600

    problems with copy-paste

--- scripts/cache_upload.sh
+++ scripts/cache_upload.sh
@@ -6,24 +6,24 @@ echo "--- Upload artifacts to cache"
 
 echo ":nix: tarballs"
 if [ -d ".nix-mix" ]; then
-  buildkite-agent artifact download cache__nix_mix.tar.gz .
+  buildkite-agent artifact upload cache__nix_mix.tar.gz
   tar -xzf cache__nix_mix.tar.gz .nix-mix
 fi
 
 echo ":elixir: tarballs"
 if [ -f "mix.lock" ]; then
-  buildkite-agent artifact download cache__deps.tar.gz .
+  buildkite-agent artifact upload cache__deps.tar.gz
   tar -xzf cache__deps.tar.gz deps
 fi
 if [ -d "_build" ]; then
-  buildkite-agent artifact download cache__build.tar.gz .
+  buildkite-agent artifact upload cache__build.tar.gz
   tar -xzf cache__build.tar.gz _build
 fi
 
 echo ":javascript: tarballs"
 cd assets
 if [ -f "yarn.lock" ] || [ -f "package-lock.json" ] || [ -f "pnpm-lock.yaml" ]; then
-  buildkite-agent artifact download cache__node_modules.tar.gz .
+  buildkite-agent artifact upload cache__node_modules.tar.gz
   tar -xzf cache__node_modules.tar.gz node_modules
 fi
 cd ..
