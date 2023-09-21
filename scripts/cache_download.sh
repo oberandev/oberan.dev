f580b66cb4b1ea9c76dca68208e248344c3b12f5
Hans Hoffman <hans@oberan.dev>
2023-09-20 22:17:31 -0600

    remove 'pkg'

--- scripts/cache_download.sh
+++ scripts/cache_download.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 echo "--- Download artifacts from cache"
 
 echo ":elixir: tarballs"
-buildkite-agent artifact download pkg/cache__build.tar.gz .
-buildkite-agent artifact download pkg/cache__deps.tar.gz .
+buildkite-agent artifact download cache__build.tar.gz .
+buildkite-agent artifact download cache__deps.tar.gz .
 tar -xzvf cache__build.tar.gz _build
 tar -xzvf cache__deps_modules.tar.gz deps
 
 echo ":javascript: tarballs"
 cd assets
-buildkite-agent artifact download pkg/cache__node_modules.tar.gz .
+buildkite-agent artifact download cache__node_modules.tar.gz .
 tar -xzvf cache__node_modules.tar.gz node_modules
