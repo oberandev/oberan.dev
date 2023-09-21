eb4d0d1774372141bdd5bb727d9341ca96cce75d
Hans Hoffman <hans@oberan.dev>
2023-09-20 22:04:06 -0600

    forgot the compilation step!

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 echo "--- Install project dependencies"
 
 echo ":elixir: Elixir deps"
-nix-shell --run "make deps-ex"
+nix-shell --run "mix deps.get"
+nix-shell --run "mix compile"
 
 echo ":yarn: Javascript deps"
-nix-shell --run "make deps-js && cd .."
+nix-shell --run "cd assets && yarn && cd .."
