d8b5d16023d20d9325401e049cddec168ea72738
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2023-09-21 15:07:00 -0600

    feat: configure sentry error reporting (#3)
    * feat: configure sentry error reporting
    
    * configure sentry for javascript
    
    minus source maps
    
    ---------
    
    Co-authored-by: Hans Hoffman <hans@oberan.dev>

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule Oberan.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
       {:plug_cowboy, "~> 2.5"},
+      {:sentry, "~> 9.0"},
       {:swoosh, "~> 1.3"},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
