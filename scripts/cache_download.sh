1fdcaba723638537c4a5f3001a19da9feba89f62
Hans Hoffman <hans@oberan.dev>
2023-09-20 21:40:06 -0600

    lets try the caching mechanism

---
+++ scripts/cache_download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- Download artifacts from cache"
+
+echo ":elixir: tarballs"
+buildkite-agent artifact download cache__build.tar.gz
+buildkite-agent artifact download cache__deps.tar.gz
+tar -xzvf cache__build.tar.gz _build
+tar -xzvf cache__deps_modules.tar.gz deps
+
+echo ":javascript: tarballs"
+cd assets
+buildkite-agent artifact download cache__node_modules.tar.gz
+tar -xzvf cache__node_modules.tar.gz node_modules
---
+++ scripts/cache_upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- Upload artifacts to cache"
+
+echo ":elixir: tarballs"
+tar -czf cache__build.tar.gz _build
+tar -czf cache__deps_modules.tar.gz deps
+buildkite-agent artifact upload cache__build.tar.gz
+buildkite-agent artifact upload cache__deps.tar.gz
+
+echo ":javascript: tarballs"
+cd assets
+tar -czf cache__node_modules.tar.gz node_modules
+buildkite-agent artifact upload cache__node_modules.tar.gz
