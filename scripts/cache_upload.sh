92e411520882f601d693e2db53db47d850542aa2
Hans Hoffman <hans@oberan.dev>
2023-09-21 09:41:36 -0600

    again...

--- scripts/cache_upload.sh
+++ scripts/cache_upload.sh
@@ -7,23 +7,23 @@ echo "--- Upload artifacts to cache"
 echo ":nix: tarballs"
 if [ -d ".nix-mix" ]; then
   buildkite-agent artifact upload cache__nix_mix.tar.gz
-  tar -xzf cache__nix_mix.tar.gz .nix-mix
+  tar -czf cache__nix_mix.tar.gz .nix-mix
 fi
 
 echo ":elixir: tarballs"
 if [ -f "mix.lock" ]; then
   buildkite-agent artifact upload cache__deps.tar.gz
-  tar -xzf cache__deps.tar.gz deps
+  tar -czf cache__deps.tar.gz deps
 fi
 if [ -d "_build" ]; then
   buildkite-agent artifact upload cache__build.tar.gz
-  tar -xzf cache__build.tar.gz _build
+  tar -czf cache__build.tar.gz _build
 fi
 
 echo ":javascript: tarballs"
 cd assets
 if [ -f "yarn.lock" ] || [ -f "package-lock.json" ] || [ -f "pnpm-lock.yaml" ]; then
   buildkite-agent artifact upload cache__node_modules.tar.gz
-  tar -xzf cache__node_modules.tar.gz node_modules
+  tar -czf cache__node_modules.tar.gz node_modules
 fi
 cd ..
