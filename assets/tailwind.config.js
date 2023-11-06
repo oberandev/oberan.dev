c9f235e5a5eea6f1df0cb39d703517f11bd461e5
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2023-11-06 15:49:22 -0500

    feat: add new fonts (#18)
    * feat: add new fonts
    
    * feat: add page titles
    
    ---------
    
    Co-authored-by: Hans Hoffman <hans@oberan.dev>

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,16 +2,15 @@
 // https://tailwindcss.com/docs/configuration
 
 const plugin = require("tailwindcss/plugin");
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
 module.exports = {
   content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
-    extend: {
-      colors: {
-        brand: "#FD4F00",
-      },
+    fontFamily: {
+      display: "Oswald",
+      sans: "Source Sans Pro",
     },
   },
   plugins: [
