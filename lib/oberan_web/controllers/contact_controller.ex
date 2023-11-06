feeda10e157afa6068d272739e76c04db303973a
Hans Hoffman <hans@oberan.dev>
2023-11-06 15:34:42 -0500

    feat: add page titles

--- lib/oberan_web/controllers/contact_controller.ex
+++ lib/oberan_web/controllers/contact_controller.ex
@@ -2,6 +2,8 @@ defmodule OberanWeb.ContactController do
   use OberanWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    conn
+    |> assign(:page_title, "Contact")
+    |> render(:home)
   end
 end
